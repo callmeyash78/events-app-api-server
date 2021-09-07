f28ebce516c95a183e7055f1486eedc3e66d7c33
callmeyash78 <call.me.yash.78@gmail.com>
2021-09-07 09:35:31 +0000

    Before pipeline

---
+++ src/routes/index.js
@@ -0,0 +1,18 @@
+/**
+ * Returns a message when the base route '/' is called
+ */
+const express = require('express');
+const router = express.Router();
+const AppConfig = require('../app.config');
+const appConfig = new AppConfig();
+
+/* GET base route. */
+router.get('/', function(req, res, next) {
+  res.json({message: `Events API Server. Version: ${appConfig.version}`});
+});
+
+router.get('/favicon.ico', (req, res) => res.status(204));
+
+module.exports = router;
+
+console.log("Some example message");
